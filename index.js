57bf4c4a83722fc4c92147603adeb5a5208b0e6c
JJ Bastien <jjbastien78@gmail.com>
2022-02-04 08:54:41 -0600

    here we go

--- index.js
+++ index.js
@@ -7,24 +7,11 @@ const app = express();
 
 const path = require ('path')
 
-
-
 // Initialize middleware
-
 app.use(express.json({extended:false}))
 app.use(cors())
 
 app.use('/posts',  require('./routes/posts'));
-
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 
 dbConnect();
