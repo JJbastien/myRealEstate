e55a5e372990a1a5c2a91475a804b82cb3949fd4
JJ Bastien <jjbastien78@gmail.com>
2022-02-04 16:04:10 -0600

    test

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,6 +1,7 @@
 import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
 
-import * as api from '../api/index.js';
+import * as api from '../api/index';
+
 
 export const getPosts = () => async (dispatch) => {
   try {
