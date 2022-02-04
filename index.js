9e0ab547604166a85057d7506ed1184d58911ee0
JJ Bastien <jjbastien78@gmail.com>
2022-02-04 02:22:53 -0600

    change to deployment

--- index.js
+++ index.js
@@ -16,9 +16,15 @@ app.use(cors())
 
 app.use('/posts',  require('./routes/posts'));
 
-app.get('/', (req, res) => {
-    res.send("Welcome to madeInPap Memories")
-})
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 
 dbConnect();
